c2c487ade843c3801fa1997676772ab71c1b69af
ENG AYUUB <moztaphem9@gmal.com>
2023-06-15 19:04:43 +0300

    get first Name done

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // Good Luck 💪🏾
+
+const getName =  ([["Ahmed","Aisha","Amina"],["Jaamac","Khadar","azma"],["Ayub","kabadhe"]]) 
+const groups = [["Ahmed","Aisha","Amina"] ,["Jaamac","khadar","Azma"], ["Ayub","kabadhe"]]
+console.log(groups[0][0],groups[1][0],groups[2][0])
